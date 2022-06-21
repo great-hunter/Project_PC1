b78455e385b90563cff9ca692f2a8edcf8c00ff6
great-hunter <greathunter@mail.ru>
2022-06-21 21:02:47 +0300

    26-33

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const contents = document.querySelectorAll('.program-line__content');
+
+contents.forEach((elem) => {
+  const title = elem.querySelector('.program-line__title');
+});
+
+
+
+
+
+// 00 : 26 : 33
