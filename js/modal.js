073e18e51f03a38938ba90cc606c2a171f30e484
great-hunter <greathunter@mail.ru>
2022-07-03 18:08:36 +0300

    trans 3 27-47

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,28 @@
 const modalBtn = document.querySelector('.modal__button');
 const modal = document.querySelector('.modal');
+const modalInner = modal.querySelector('.modal__inner');
+
+modalInner.style.position = 'relative';
+
+const addCloseBtn = () => {
+  const closeBtn = document.createElement('div');
+  closeBtn.classList.add('close-button');
+  closeBtn.innerHTML = '&times';
+  closeBtn.style.position = 'absolute';
+  closeBtn.style.right = '10px';
+  closeBtn.style.top = '10px';
+  closeBtn.style.padding = '3px';
+  closeBtn.style.width = '20px';
+  closeBtn.style.height = '20px';
+  closeBtn.style.fontSize = '20px';
+  closeBtn.style.cursor = 'pointer';
+
+  modalInner.append(closeBtn);
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = '';
+  });
+}
 
 modalBtn.addEventListener('click', () => {
   modal.style.display = 'flex';
@@ -11,3 +34,5 @@ modal.addEventListener('click', (event) => {
     modal.style.display = '';
   }
 })
+
+addCloseBtn();
