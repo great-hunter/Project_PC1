384d57d98074d6c06118baa70485479b50c86840
great-hunter <greathunter@mail.ru>
2022-07-13 17:36:54 +0300

    lesson 4 / 03:08

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,23 +1,27 @@
-const contents = document.querySelectorAll('.program-line__content');
-const contentsDescr = document.querySelectorAll('.program-line__descr');
+const accordeon = () => {
+  const contents = document.querySelectorAll('.program-line__content');
+  const contentsDescr = document.querySelectorAll('.program-line__descr');
 
-contents.forEach((elem) => {
-  const title = elem.querySelector('.program-line__title');
-  const descr = elem.querySelector('.program-line__descr');
+  contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title');
+    const descr = elem.querySelector('.program-line__descr');
 
-  descr.style.transition = 'all .3s';
-  descr.style.overflow = 'hidden';
+    descr.style.transition = 'all .3s';
+    descr.style.overflow = 'hidden';
 
-  title.addEventListener('click', () => {
-    contentsDescr.forEach((tab) => {
-      if (tab === descr) {
-      //  tab.classList.add('active');
-        tab.style.height = tab.scrollHeight + 'px';
-      } else {
-       // tab.classList.remove('active');
-        tab.style.height = 0;
-      }
+    title.addEventListener('click', () => {
+      contentsDescr.forEach((tab) => {
+        if (tab === descr) {
+          //  tab.classList.add('active');
+          tab.style.height = tab.scrollHeight + 'px';
+        } else {
+          // tab.classList.remove('active');
+          tab.style.height = 0;
+        }
+      });
     });
+
   });
-  
-});
+}
+
+accordeon();
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,17 +1,21 @@
-const navbar = document.querySelector('.header__nav');
-const links = navbar.querySelectorAll('a');
+const smoothScroll = () => {
+  const navbar = document.querySelector('.header__nav');
+  const links = navbar.querySelectorAll('a');
 
-links.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    const section = document.querySelector(link.getAttribute('href'));
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const section = document.querySelector(link.getAttribute('href'));
 
-    if (section) {
-      seamless.scrollIntoView(section, {
-        behavior: "smooth",
-        block: "start",
-        inline: "center",
+      if (section) {
+        seamless.scrollIntoView(section, {
+          behavior: "smooth",
+          block: "start",
+          inline: "center",
+        });
+      }
     });
-    }
   });
-});
+}
+
+smoothScroll();
